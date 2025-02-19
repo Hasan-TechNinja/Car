940f875629eaa2a1fd7bbc2744526387921ae353
Hasan-TechNinja <hasantechninja@gmail.com>
2025-02-19 17:08:55 +0600

    mobile get put and delete complete

--- Mobile/urls.py
+++ Mobile/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('brand/', views.BrandView.as_view(), name='brand'),
     path('mobile/', views.MobileView.as_view(), name='mobile'),
+    path('mobiledetails/<int:pk>', views.MobileDetails.as_view(), name='mobile'),
 ]
--- Mobile/views.py
+++ Mobile/views.py
@@ -4,7 +4,7 @@ from .serializers import BrandSerializers, MobileSerializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -36,3 +36,23 @@ class MobileView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class MobileDetails(APIView):
+    def get(self, request, pk):
+        mobile = get_object_or_404(Mobile, pk=pk)
+        serializer = MobileSerializers(mobile)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        mobile = Mobile.objects.get(pk = pk)
+        serializer = MobileSerializers(mobile, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        mobile = get_object_or_404(Mobile, pk=pk)
+        mobile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
